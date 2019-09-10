e6db891108f917a5c1376739571ec6762a066f98
Davi Sampaio <sampaiodev23@gmail.com>
2019-09-10 10:41:00 -0300

    tela de login e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)}, 
+  { path: 'register', loadChildren: './authentication/register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './authentication/login/login.module#LoginPageModule' },
+
 ];
 
 @NgModule({
---
+++ src/app/authentication/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+//import {Observable} from 'rxjs/Rx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService { 
+  register(valor): Observable<any>{
+    
+  }
+
+  login(valor): Observable<any>{
+    
+  }
+
+}
